850f2bf11b2acdf395d40a92693d07d845272b28
Tim Riffe <tim.riffe@gmail.com>
2015-01-19 22:37:56 -0800

    minor edits

--- YearsLost/R/ThanoPop.R
+++ YearsLost/R/ThanoPop.R
@@ -52,3 +52,65 @@ dev.off()
 
 
 
+
+dx2ex <- function(dx){
+	a <- 1:length(dx)-.5
+	fya <- da2fya(dx)
+	colSums(t(fya)*a)
+}
+# PARYL
+sum((Pxm * dx2ex(dxm))) / sum(Pxm)
+
+Pym <- rowSums(Thano(Pxm, dxm,stagger=FALSE))
+sum(Pym * .5:110.5) / sum (Pym)
+
+# Equal
+
+years <- unique(Dx$Year)
+year <- 2010
+Pex15 <- function(year, Dx,Ex,Px){
+	Dxm <- Dx$Male[Dx$Year == year]
+	Exm <- Ex$Male[Ex$Year == year]
+	Dxf <- Dx$Female[Dx$Year == year]
+	Exf <- Ex$Female[Ex$Year == year]
+	Pxm <- Px$Male1[Px$Year == year]
+	Pxf <- Px$Female1[Px$Year == year]
+	
+	Mxm <- Dxm / Exm
+	Mxf <- Dxf / Exf
+	
+	lxm <- c(1, exp(-cumsum(Mxm)))
+	lxf <- c(1, exp(-cumsum(Mxf)))
+	
+	dxm <- lx2dx(lxm)
+	dxf <- lx2dx(lxf)
+	
+	Pym <- rowSums(Thano(Pxm, dxm))
+	Pyf <- rowSums(Thano(Pxf, dxf))
+	
+	Py15 <- sum(c(Pym[1:15],Pyf[1:15])) / sum(Pym+Pyf)
+	
+	exm <- dx2ex(dxm)
+	exf <- dx2ex(dxf)
+	
+	threshm <- approx(exm,0:110,xout=15)$y
+	threshf <- approx(exf, 0:110, xout=15)$y
+	intm <- floor(threshm)
+	intf <- floor(threshf)
+	
+	diffm <- threshm - intm
+	difff <- threshf - intf
+	
+	numm <- sum(Pxm[1:(intm+1)]) + diffm * Pxm[intm+2]
+	numf <- sum(Pxf[1:(intf+1)]) + difff * Pxf[intf+2]
+	
+	PLE15 <- 1 - (numm + numf) / sum(Pxm+Pxf)
+	c(Py15=Py15,PLE15=PLE15)
+}
+
+Out <- sapply(years, Pex15, Dx=Dx, Ex=Ex, Px=Px)
+
+plot(years, Out[1,], type='l',ylim=c(0,.3))
+lines(years, Out[2,],col="red")
+
+plot(Out[1,],Out[2,],asp=1)
