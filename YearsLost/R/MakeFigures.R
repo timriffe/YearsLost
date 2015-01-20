d9341e4535cae9e8e9fe665763e1d6a6276d0248
Tim Riffe <tim.riffe@gmail.com>
2015-01-20 11:29:34 -0800

    a unified make script for all figures. Will save time in a simple reproduction.

---
+++ YearsLost/R/MakeFigures.R
@@ -0,0 +1,23 @@
+# for Tim, this will choke
+if (system("hostname",intern=TRUE) %in% c("triffe-N80Vm", "tim-ThinkPad-L440")){
+	# if I'm on the laptop
+	setwd("/home/tim/git/YearsLost/YearsLost")
+} else {
+	# in that case I'm on Berkeley system, and other people in the dept can run this too
+	setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/YearsLost/YearsLost"))
+}
+# get plotting functions
+source("R/Functions.R")
+
+
+# life line Figure (1)
+source("R/LifeLine.R")
+source("R/fya.R")
+
+
+
+
+
+
+
+
